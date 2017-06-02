91f78b0c71b1f4c9993f883751a382dcdcae6544
David Fallas <davidfa@davidfa-thi-m.cmass.criticalmass.com>
2017-06-02 08:58:29 -0600

    confict fix

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,11 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./header.component.sass']
-=======
   styleUrls: ['./header.component.css']
->>>>>>> parent of 2841398... Adding Sass and removing Testing files
 })
 export class HeaderComponent implements OnInit {
 
